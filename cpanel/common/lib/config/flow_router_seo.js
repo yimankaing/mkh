5112078083613585cbc6c3c72b9d227120f2fc56
yimankaing <yimankaing@gmail.com>
2017-11-30 10:18:17 +0700

    remove invoice footer

--- cpanel/common/lib/config/flow_router_seo.js
+++ cpanel/common/lib/config/flow_router_seo.js
@@ -2,7 +2,7 @@ SEO = new FlowRouterSEO();
 
 SEO.setDefaults({
     title: 'MKH',
-    description: 'MKH, Tel: 0123456789',
+    description: 'MKH',
     meta: {
         'property="og:type"': 'website',
         'property="og:site_name"': 'Default site name',
